25024ea8d1eb3efc440529b9e825467b2932e722
Ahmed Mostafa <ahmed.moustafa9663@gmail.com>
2020-12-21 14:48:06 +0200

    modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const FEATURED_API =
 const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-function App() {
+function App() {  
   const [movies, setMovies] = useState([]);
   const [SearchTerm, setSearchTerm] = useState("");
   useEffect(() => {
